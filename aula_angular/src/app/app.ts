d4604eda8a698c9cd6cda05938892c313f7f2eb4
Henrique Galvão Dall Alba <henriquegdall@gmail.com>
2026-07-03 17:13:39 -0300

    atualização

--- aula_angular/src/app/app.ts
+++ aula_angular/src/app/app.ts
@@ -4,10 +4,11 @@ import { EnderecoComponent } from "./endereco-component/endereco-component";
 import { PessoaComponent } from "./pessoa-component/pessoa-component";
 import { MercadoComponent } from "./mercado-component/mercado-component";
 import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EnderecoComponent, PessoaComponent, MercadoComponent, RouterLinkWithHref, Header,],
+  imports: [RouterOutlet, EnderecoComponent, PessoaComponent, MercadoComponent, RouterLinkWithHref, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
