4d63b79759448837a30241cf29f64487856b37d3
Henrique Galvão Dall Alba <henriquegdall@gmail.com>
2026-07-29 17:12:06 -0300

    Permitindo a criação de mais cards

--- aula_angular/src/app/feats/usuarios/usuarios.ts
+++ aula_angular/src/app/feats/usuarios/usuarios.ts
@@ -1,27 +1,52 @@
 import { Component, signal } from '@angular/core';
 import { Users } from './users';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-usuarios',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './usuarios.html',
   styleUrl: './usuarios.css',
 })
 export class Usuarios {
-users() {
-throw new Error('Method not implemented.');
-}
+  novoId = signal<number>(0);
+  novoNome = signal<string>('');
+  novoIdade = signal<number|null>(null);
+
+
   usuarios = signal<Users[]>([
     {
-      id:1,
-      nome:"henrique",
-      idade:70
+      id: 1,
+      nome: "henrique",
+      idade: 70
     },
     {
-      id:2,
-      nome:"henricão",
-      idade:100
+      id: 2,
+      nome: "henricão",
+      idade: 100
+    },
+  ]);
+
+  adicionarUsuario() {
+    if (this.novoNome().trim() === '' || this.novoIdade() === null) {
+      alert('Valores invalidos');
+      return;
     }
-  ]
-  );
+
+  const listaAtual = this.usuarios();
+
+    const proximoId = listaAtual.length > 0 ? listaAtual[listaAtual.length - 1].id + 1 : 1;
+
+
+    const novoUsuario: Users = {
+      id: proximoId,
+      nome: this.novoNome(),
+      idade: this.novoIdade()!
+    };
+
+    this.usuarios.update(listaAtual => [...listaAtual, novoUsuario]);
+    this.novoId.set(proximoId);
+    this.novoNome.set('');
+    this.novoIdade.set(null);
+  }
 }
