ced2f23d16f61900c84679da0bdfb62a69105c0f
Henrique Galvão Dall Alba <henriquegdall@gmail.com>
2026-08-11 17:28:50 -0300

    atualizando

--- aula_angular/src/app/feats/media-alunos/media-alunos.ts
+++ aula_angular/src/app/feats/media-alunos/media-alunos.ts
@@ -1,9 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Aluno } from './aluno';
+
 
 @Component({
-  selector: 'app-media-alunos',
-  imports: [],
+  selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './media-alunos.html',
   styleUrl: './media-alunos.css',
 })
-export class MediaAlunos {}
+export class MediaAlunos {
+[x: string]: any;
+   nome = signal('');
+  media = signal<number | null>(null);
+  alunos = signal<Aluno[]>([]);
+  notaInvalida = signal(false);
+  nomeInvalido = signal(false);
+
+  cadastrar() {
+
+    const nomeAluno = this.nome().trim();
+    const mediaAluno = this.media();
+
+
+
+    if (nomeAluno === '') {
+      this.nomeInvalido.set(true)
+      return;
+    }
+        else{
+      this.nomeInvalido.set(false)
+    }
+
+    if (mediaAluno === null) {
+      this.notaInvalida.set(true)
+      
+      return;
+    }
+    else{
+      this.notaInvalida.set(false)
+    }
+
+    if (mediaAluno < 0 || mediaAluno > 10) {
+      this.notaInvalida.set(true)
+      return;
+    }
+        else{
+      this.notaInvalida.set(false)
+    }
+
+    this.alunos.update(lista => [
+      ...lista,
+      {
+        nome: nomeAluno,
+        media: mediaAluno
+      }
+    ]);
+
+    this.nome.set('');
+    this.media.set(null);
+  }
+}
