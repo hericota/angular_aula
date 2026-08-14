425559bf1c56b8525b5ecaa1d39f87c44f6359ee
Henrique Galvão Dall Alba <henriquegdall@gmail.com>
2026-08-14 17:18:29 -0300

    atualização

--- aula_angular/src/app/feats/login/login.ts
+++ aula_angular/src/app/feats/login/login.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { FormField } from "@angular/forms/signals";
+import { form, FormField } from "@angular/forms/signals";
 import { Usuario } from './usuario';
 
 @Component({
@@ -9,9 +9,48 @@ import { Usuario } from './usuario';
   styleUrl: './login.css',
 })
 export class Login {
-  UsuarioModel = signal<Usuario>(
-  nome: "",
-  email: "",
-  senha: "",
-  data: string )
+  nomeCorreto = signal<boolean>(true);
+  emailCorreto = signal<boolean>(true);
+  senhaCorreta = signal<boolean>(true);
+  confirmaSenhaCorreta = signal<boolean>(true);
+
+
+  UsuarioModel = signal<Usuario>({
+    nome: "",
+    email: "",
+    senha: "",
+    confirmaSenha: "",
+    data: "",
+  })
+  login = form(this.UsuarioModel, (s) => {
+  })
+  protected usuarios = signal<Usuario[]>([]);
+  cadastrar(event: Event) {
+    event.preventDefault();
+
+    this.nomeCorreto.set(this.UsuarioModel().nome !== "");
+
+    this.emailCorreto.set(this.UsuarioModel().email !== "");
+
+    this.senhaCorreta.set(this.UsuarioModel().senha !== "");
+
+    this.confirmaSenhaCorreta.set(
+      this.UsuarioModel().confirmaSenha !== "" &&
+      this.UsuarioModel().confirmaSenha === this.UsuarioModel().senha
+    );
+
+    if (
+      this.nomeCorreto() &&
+      this.emailCorreto() &&
+      this.senhaCorreta() &&
+      this.confirmaSenhaCorreta()
+    ) {
+      const usuario = this.UsuarioModel();
+
+      this.usuarios.update(valor => [...valor, usuario]);
+    }
+  }
+
+
+
 }
--- aula_angular/src/app/feats/login/usuario.ts
+++ aula_angular/src/app/feats/login/usuario.ts
@@ -2,6 +2,7 @@ export interface Usuario {
     nome: string;
     email:string;
     senha:string;
+    confirmaSenha:string;
     data:string;
 
 }
